7b296372b558cdf738b591cfeeaa0836eb13b7bc
John Ghatas <johnghatas97@hotmail.com>
2023-06-04 13:40:45 +0200

    rebuild(part 3): Switch to a simpler boilerplate

--- app/src/routes/address.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import addressController from "../controllers/address.controller.js";
-
-const addressRoutes = Router();
-addressRoutes.post("/address", addressController.add);
-
-export { addressRoutes };
